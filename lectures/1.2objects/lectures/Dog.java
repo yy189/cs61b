685401887876ea62eb2050559ddab834a72de510
yy189 <yyuan@cs.duke.edu>
2018-04-21 12:51:38 -0400

    hiiii

---
+++ lectures/1.2objects/lectures/Dog.java
@@ -0,0 +1,17 @@
+public class Dog {
+	public int weightInPounds;
+
+	public Dog(int w) {
+		weightInPounds = w;
+	}
+
+	public void makeNoise() {
+		if (weightInPounds < 10) {
+			System.out.println("yip!");
+		} else if (weightInPounds < 30) {
+			System.out.println("bark.");
+		} else {
+			System.out.println("woooof!");
+		}
+	}
+}
